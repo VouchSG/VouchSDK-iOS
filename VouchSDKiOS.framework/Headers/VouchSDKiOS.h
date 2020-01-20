78c02e4d4e40915a74b62b7e8c9db174e1004a64
Ajie Pramono Arganata <ajie@gits.co.id>
2020-01-20 14:55:42 +0700

    Change log: - Change module name into VouchSDKiOS - Fixing on framework asset new_loading.json doesnt loaded - Fixing insert new message from socket after app goes to foreground and become active again - Fixing title button greeting

---
+++ VouchSDKiOS.framework/Headers/VouchSDKiOS.h
@@ -0,0 +1,19 @@
+//
+//  VouchSDKiOS.h
+//  VouchSDKiOS
+//
+//  Created by Ajie Pramono Arganata on 20/01/20.
+//  Copyright © 2020 Ajie Pramono Arganata. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VouchSDKiOS.
+FOUNDATION_EXPORT double VouchSDKiOSVersionNumber;
+
+//! Project version string for VouchSDKiOS.
+FOUNDATION_EXPORT const unsigned char VouchSDKiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VouchSDKiOS/PublicHeader.h>
+
+
